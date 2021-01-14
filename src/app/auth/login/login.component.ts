37412f8dae743ffa6109d9deeef2147883775678
Richard3 <rechard.the3@gmail.com>
2021-01-14 17:09:41 +0300

    added fancy login with fb buttons

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserForm } from '../forms/user.form';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor(public formDef: UserForm, private fb: FormBuilder) { }
+  constructor(
+    public formDef: UserForm,
+    private fb: FormBuilder,
+    private snackbar: MatSnackBar
+  ) {}
 
   form = this.fb.group({});
   model = {};
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  loginWithFacebook() {
+    this.snackbar.open("This feature is not implemented just yet", "OK");
   }
 
+  login(){
+    this.snackbar.open("This feature is not implemented just yet", "OK");
+  }
+
+  forgotPassword(){
+    this.snackbar.open("This feature is not implemented just yet", "OK");
+  }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormsService } from 'src/app/core/forms/forms.service';
 import { UserForm } from '../forms/user.form';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-
-  constructor(public formDef: UserForm, private fb: FormBuilder) {
-
-  }
+  constructor(
+    public formDef: UserForm,
+    private fb: FormBuilder,
+    private snackbar: MatSnackBar
+  ) {}
   form = this.fb.group({});
   model = {};
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  signUp() {}
+
+  loginFacebook() {
+    this.snackbar.open('This feature is not ready just yet', 'OK', {
+      duration: 2000,
+    });
+  }
 }
